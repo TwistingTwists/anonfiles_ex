f2da5e7e8c0137766fa6acff2cd94207b1bce45c
Abhishek <abhishek.tripathi456@gmail.com>
2022-08-06 03:35:38 +0530

    fixed elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AnonDown.MixProject do
     [
       app: :anon_down,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
